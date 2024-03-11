92d084d8d5d71b4298d6192621b32718df8c8734
Pulado <143140211+Pulado@users.noreply.github.com>
2024-03-11 16:29:34 -0300

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,16 @@
+function TemaEscuro(){
+
+    document.body.style.backgroundColor="#151515" 
+    document.body.style.color="white"
+    
+    }
+    
+    function Temaclaro(){
+    
+    document.body.style.backgroundColor="#D88810"
+    
+    document.body.style.color="#FFF0DD"
+    }
+
+    
+    
